ea354b20eb638a16762c5e0336620f710a8e701d
sycbelief <syclxzy@126.com>
2014-07-17 11:04:46 +0800

    函数框架

---
+++ main.py
@@ -0,0 +1,25 @@
+def filter(long_string):
+
+    return stringList
+
+def findPos(short_string):
+
+    return posList
+
+def calFreq():
+
+    return
+
+def calDOF():
+
+    return
+
+def calDoc():
+
+    return
+
+
+def main():
+
+
+    return 0
