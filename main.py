5c2963f47cecb844654989389f0e8cad70eaaed1
wh61 <fx040308wh@gmail.com>
2014-07-18 13:29:01 +0800

    change the main.py

--- main.py
+++ main.py
@@ -1,53 +1,104 @@
-import string
+#import string
 import math
 
-def filter(long_string):
+"""def filter(long_string):
 	delList = string.punctuation+" '《》（）&%￥#@！{}【】？。，—_“”：；、<>"
 	t = long_string.maketrans(delList, ''*len(delList))
 	stringList = long_string.translate(t)
-	return stringList3
+	return stringList3"""
 
-def findPos(clean_string):
+I2C = ["" for i in range(25000)]
 
-    return posList
+def charactor2Int(charactor):
+        bys = charactor.encode('utf-8')
+        if (len(bys) != 2): return -1;
+        if (not((bys[0] > 129 and bys[0] < 255) and
+                        (bys[1] > 63 and bys[1] < 255 ))): return -1
+        by0 = int(bys[0])
+        by1= int(bys[1])
+        hs = (by0 - 129) * 190 + (by1 - 64) - by1 // 128
+        if (hs < 9026): return -1
+        return hs - 9025
+
+def WashTheTexts_(inputStr, int2charactor):
+        arr = []
+        ResultStr = ""
+        for ch in inputStr:
+                hashValue = charactor2Int(ch)
+                if (hashValue < 0): continue
+                arr.append(hashValue)
+                ResultStr += ch
+                int2charactor[hashValue] = ch
+        return ResultStr
+
+
+def WashTheTexts(inputStr, int2charactor = I2C):
+        return WashTheTexts_(inputStr, I2C)
+
+def getCharator(index, arr = I2C):
+        return I2C[index]
+
+def findPos(texts, d):
+    s = texts
+    strLen = len(s)
+    for i in range(2 * (d + 1)): s += "ä¸"
+    dic = {}
+    for i in range(strLen):
+        Str = ""
+        for j in range(d + 1):
+            Str += s[i + j]
+        dic[Str] = i
+    keyList = sorted(dic)
+    pos = []
+    for key in keyList:
+        pos.append(dic[key])
+    return pos
 
 def calDOF(dic):
-	freq = dic.values()
-	sumOfFreq = sum(freq)
-	func = lambda x: -x/sumOfFreq*math.log(x/sumOfFreq)
-	return sum(list(map(func, freq)))
+        #print(dic)
+        #if len(dic) == 1:
+        #        return 0
+        freq = dic.values()
+        sumOfFreq = sum(freq)
+        func = lambda x: -x/sumOfFreq*math.log(x/sumOfFreq)
+        return sum(list(map(func, freq)))
 
 def calFreq_RDOF_DOC(clean_string, posList, DigLen):
-	
+	#print (clean_string, posList, DigLen)
 	freqDic = {}
 	RDOFdic = {}
 	length = len(clean_string)
 	for m in range(1, DigLen+1):
 		for i in posList:
-			if i > length - m:
+			if i+m > length:
 				continue
 			else:
 				target = clean_string[i:i+m]
+				#print ("target: ", target)
+				#print ("length: ", length)
+				#print ("i: ", i)
+				#print ("m: ", m)
 				if target not in freqDic:
 					freqDic[target] = 1
 					RDOFdic[target] = {}
-					if i >= length + m:
+					if i+m == length:
 						continue
 					else:
 						RDOFdic[target][clean_string[i+m]] = 1
 				else:
 					freqDic[target] += 1
-					if i >= length + m:
+					if i+m == length:
 						continue
+					if clean_string[i+m] not in RDOFdic[target]:
+						RDOFdic[target][clean_string[i+m]] = 1
 					else:
-						if clean_string[i+m] not in RDOFdic[target]:
-							RDOFdic[target][clean_string[i+m]] = 1
-						else:
-							RDOFdic[target][clean_string[i+m]] += 1
+						RDOFdic[target][clean_string[i+m]] += 1
 	RDOF = {}
 	DOC = {}
+	#print(freqDic)
 	for key in RDOFdic:
 		RDOF[key] = calDOF(RDOFdic[key])
+		#print (key)
 		if len(key) == 1:
 			DOC[key] = 1
 		elif len(key) == 2:
@@ -60,7 +111,7 @@ def calFreq_RDOF_DOC(clean_string, posList, DigLen):
 			DOC[key] = freqDic[key]*length/freqDic[key[0]]/freqDic[key[1]]
 		elif DigLen > 2:
 			tmp = 1000000
-			for t in range(len(key)):
+			for t in range(len(key)-1):
 				key1 = key[0:t+1]
 				key2 = key[t+1:]
 				#if key1 not in freqDic:
@@ -78,18 +129,18 @@ def calLDOF(clean_string, posList, DigLen):
 	length = len(clean_string)
 	for m in range(1, DigLen+1):
 		for i in posList:
-			if i > length - m:
+			if i + m > length:
 				continue
 			else:
 				target = clean_string[i:i+m]
 				if target not in LDOFdic:
 					LDOFdic[target] = {}
-					if i >= length + m:
+					if i + m == length:
 						continue
 					else:
 						LDOFdic[target][clean_string[i+m]] = 1
 				else:
-					if i >= length + m:
+					if i + m == length:
 						continue
 					else:
 						if clean_string[i+m] not in LDOFdic[target]:
@@ -105,23 +156,36 @@ def calLDOF(clean_string, posList, DigLen):
 def main(DigLen):
 	# DigLen表示最长码长
 	#假定有5篇文本，命名一次为1.txt, 2.txt,...
-	filename = ".txt"
+	filename = "gongchengche"
 	long_string = ""
-	for i in range(5):
-		f = open(str(i+1)+filename, 'r', encoding = "gbk")
+	for i in range(1):
+		f = open(filename, 'r', encoding = "utf-8")
 		for line in f:
+			#print (line)
+			#break
 			if line[-1] == "\n":
 				line = line[:-1]
 			long_string += line
 		f.close()
-
+	clean_string = WashTheTexts(long_string)
+	print(long_string)
 	#stringList = filter(long_string)
 
-	posList = findPos(clean_string)
+	posList = findPos(clean_string, DigLen)
 	freqDic, RDOF, DOC = calFreq_RDOF_DOC(clean_string, posList, DigLen)
 	r_clean_string = clean_string[::-1]
-	r_postList = findPos(r_clean_string)
+	r_posList = findPos(r_clean_string, DigLen)
 	LDOF = calLDOF(r_clean_string, r_posList, DigLen)
-
+	print ("freqDic: ", freqDic)
+	print ("RDOF: ", RDOF)
+	print ("LDOF: ", LDOF)
+	print ("DOC: ", DOC)
+	"""for word in freqDic.keys():
+		if freqDic[word] > 2:
+			if min(RDOF[word], LDOF[word]) > 0.5:
+				if DOC[word] > 0.5:
+					print(word)"""
+                                
 	return 0
 
+main(5)
